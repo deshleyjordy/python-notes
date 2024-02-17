d8dca0b64fe49cd5fc7682d066c19a266160da37
Deshley <elon.dusk179@passinbox.com>
2024-02-18 00:10:55 +0100

    Add switcher

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,3 +1,7 @@
+rules = [
+    [0, -1, 1],
+]
+
 class Participant:
     def __init__(self, name):
         self.name = name
@@ -6,13 +10,21 @@ class Participant:
     def choose(self):
         self.choice = input("{name}, select rock, paper or scissor: ".format(name = self.name))
         print("{name} selects {choice}".format(name = self.name, choice = self.choice))
+    def toNumericalChoice(self):
+        switcher = {
+            "rock": 0,
+            "paper": 1,
+            "scissor": 2
+        }
+        return switcher[self.choice]
+        
 
 class GameRound:
     def __init__(self, p1, p2):
         p1.choose()
         p2.choose()
-    def compareChoices(self):
-        print("implement")
+    def compareChoices(self, p1, p2):
+        return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
     def awardPoints(self):
         print("implement")
 
